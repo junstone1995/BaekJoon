35e438d323ce5afa44eb5d4e72bc4bbbc20a2da7
junstone1995 <boris0730@gmail.com>
2020-01-02 16:06:33 +0900

    20200102 백준알고리즘

---
+++ 1316.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define max 101
+int main(void)
+{
+    int i,cnt,count = 0;
+    char word[max];
+    scanf("%d",&cnt);
+    for(i=0;i<cnt;i++)
+        {scanf("%s",word);
+        for(j=0; j<sizeof(word);j++)
+            dfdf
+        }
+}

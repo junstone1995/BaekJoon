de0e376cc346631f909c97bd141d3538049afbf3
junstone1995 <boris0730@gmail.com>
2020-01-27 16:52:38 +0900

    백준알고리즘

---
+++ Java/src/Java10250.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Java10250 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testcase = sc.nextInt();
+        for (int i = 0; i < testcase; i++) {
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            int n = sc.nextInt();
+            int floor = n%h, room = n/h+1;
+            if (n%h ==0){
+                room= n/h;
+                floor = h;
+            }
+            System.out.println(floor*100+room);
+        }
+    }
+}

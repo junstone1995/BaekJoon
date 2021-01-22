bfb3344dd5e480bc56bbe4c1b848ffe14aaf8c22
이준석 <boris0730@gmail.com>
2021-01-22 15:02:53 +0900

    11653

--- c++/11653(소인수분해)/11653.cpp
+++ c++/11653(소인수분해)/11653.cpp
@@ -2,46 +2,22 @@
 
 using namespace std;
 
-int        prime(int nb)
+int		main()
 {
-    long long i;
+	int N;
 
-    i = 2;
-    if (nb <= 1)
-        return (0);
-    while (i * i <= nb)
-    {
-        if (nb % i == 0)
-            return (0);
-        i++;
-    }
-    return (1);
-}
-
-int        main()
-{
-    int N;
-
-    cin >> N;
-    if (N == 1)
-        return (0);
-    for (int i = 2; i < N; i++)
-    {
-        if (prime(i))
-        {
-            while (N > 1)
-            {
-                if (N % i == 0)
-                {
-                    cout << i << endl;
-                    N /= i;
-                }
-                else
-                    break;
-            }
-        }
-    }
-    if (N > 1)
-        cout << N << endl;
-    return (0);
+	cin >> N;
+	for (int i = 2; N > 1;)
+	{
+		if (N % i == 0)
+		{
+			cout << i << endl;
+			N /= i;
+		}
+		else
+			i++;
+	}
+	if (N > 1)
+		cout << N << endl;
+	return (0);
 }

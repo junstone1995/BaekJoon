2311316f34e4f1d59b106c1675cd4036e78089f7
이준석 <boris0730@gmail.com>
2021-01-22 15:57:32 +0900

    11653

---
+++ c++/1929(소수 구하기)/1929.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int	main(void)
+{
+	int max,min;
+	cin >> min >> max;
+	bool *prime = new bool[max + 1];
+	fill_n(prime, max+1, true);
+	prime[0] = false;
+	prime[1] = false;
+	for (int i = 2; i <= sqrt(max); i++)
+		if(prime[i] == true)
+			for (int j = i * 2; j <= max; j += i)
+				prime[j] = false;
+	for (int i = min; i <= max; i++)
+		if(prime[i] == true)
+			cout << i << "\n";
+	delete[] prime;
+	return (0);
+}

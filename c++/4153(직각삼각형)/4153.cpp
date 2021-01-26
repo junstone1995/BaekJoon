18b70ddd2871678d427e73dcd0c655fa0ce09382
이준석 <boris0730@gmail.com>
2021-01-26 18:43:24 +0900

    4153

---
+++ c++/4153(직각삼각형)/4153.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	while(1)
+	{
+		int x,y,z;
+		cin >> x >> y >> z;
+		if (x == 0 && y == 0 && z == 0)
+			break;
+		else if (z *z == x * x + y * y)
+			cout << "right" << '\n';
+		else if (y * y == x * x + z * z)
+			cout << "right" << '\n';
+		else if (x * x == y * y + z * z)
+			cout << "right" << '\n';
+		else
+			cout << "wrong" << '\n';
+	}
+	return (0);
+}

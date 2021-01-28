63594e975b7d596ad0c96e3e4cc8201d475adb4f
이준석 <boris0730@gmail.com>
2021-01-29 02:28:58 +0900

    1018

---
+++ c++/1018(체스판 다시 칠하기)/1018.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string WB[8] = {
+	"WBWBWBWB",
+	"BWBWBWBW",
+	"WBWBWBWB",
+	"BWBWBWBW",
+	"WBWBWBWB",
+	"BWBWBWBW",
+	"WBWBWBWB",
+	"BWBWBWBW"
+};
+
+string BW[8] = {
+	"BWBWBWBW",
+	"WBWBWBWB",
+	"BWBWBWBW",
+	"WBWBWBWB",
+	"BWBWBWBW",
+	"WBWBWBWB",
+	"BWBWBWBW",
+	"WBWBWBWB"
+};
+
+int main()
+{
+	int N,M;
+	string board[50];
+	cin >> N >> M;
+	for (int i = 0 ; i < N; i++)
+		cin >> board[i];
+	for (int i = 0; i + 8 <= )
+}

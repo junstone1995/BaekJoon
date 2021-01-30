01d634c13785a4905e50516cb696f204de6a4ef7
이준석 <boris0730@gmail.com>
2021-01-30 18:54:04 +0900

    1018

--- c++/1018(체스판 다시 칠하기)/1018.cpp
+++ c++/1018(체스판 다시 칠하기)/1018.cpp
@@ -25,12 +25,35 @@ string BW[8] = {
 	"WBWBWBWB"
 };
 
+string board[50];
+
+int whiteFirst(int y,int x)
+{
+	int cnt = 0;
+	for (int i = y; i < y + 8 ; i++)
+		for (int j = x; j < x + 8; j++)
+			if (board[i][j] != WB[i-y][j-x])
+				cnt++;
+}
+
+int blackFirst(int y,int x)
+{
+	int cnt = 0;
+	for (int i = y; i < y + 8 ; i++)
+		for (int j = x; j < x + 8; j++)
+			if (board[i][j] != BW[i-y][j-x])
+				cnt++;
+}
 int main()
 {
-	int N,M;
-	string board[50];
+	int N,M,ret;
 	cin >> N >> M;
 	for (int i = 0 ; i < N; i++)
 		cin >> board[i];
-	for (int i = 0; i + 8 <= )
+	ret = 2147483647;
+	for (int i = 0; i + 7 < N; i++)
+		for(int j = 0; j + 7 < M; j++)
+			ret = min(ret , min(whiteFirst(i,j),blackFirst(i,j)));
+	cout << ret << '\n';
+	return (0);
 }

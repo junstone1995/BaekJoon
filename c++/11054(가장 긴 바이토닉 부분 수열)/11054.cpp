0ccbd1a571be6f184ab411d919878ab66acf8548
이준석 <boris0730@gmail.com>
2021-02-19 16:55:36 +0900

    11054

--- c++/11054(가장 긴 바이토닉 부분 수열)/11054.cpp
+++ c++/11054(가장 긴 바이토닉 부분 수열)/11054.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 
-using namespace ste;
+using namespace std;
+
+int n;
+int arr[1001];
+int dp[2][1001];
 
 int main()
 {
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> arr[i];
+	for (int i = 1; i <= n; i++)
+	{
+		dp[0][i] = 1;
+		for (int j = 1; j <= i; j++)
+		{
+			if(arr[j] < arr[i] && dp[0][i] < dp[0][j] + 1)
+				dp[0][i] = dp[0][j] + 1;
+		}
+	}
 
+	for (int i = n; i >= 1; i--)
+	{
+		dp[1][i] = 1;
+		for (int j = n; j >= i; j--)
+		{
+			if (arr[i] > arr[j] && dp[1][j] + 1 > dp[1][i])
+				dp[1][i] = dp[1][j] + 1;
+		}
+	}
+	int ret = 0;
+	for (int i = 0; i <= n; i++)
+	{
+		if (ret < dp[0][i] + dp[1][i] - 1)
+			ret = dp[0][i] + dp[1][i] - 1;
+	}
+	cout << ret << '\n';
 }

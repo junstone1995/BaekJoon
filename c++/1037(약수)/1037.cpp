4dcd242e7a9f08bb2b1c84bc34bf41ec7d303f25
이준석 <boris0730@gmail.com>
2021-03-02 16:28:07 +0900

    1037

---
+++ c++/1037(약수)/1037.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+    int* div = new int[n];
+    for(int i = 0; i < n; i++)
+        cin >> div[i];
+    sort(div, div+n);
+    cout << div[0] * div[n-1];
+    delete[] div;
+}

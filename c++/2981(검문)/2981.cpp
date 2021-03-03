f37aee60b49ec45e7ec37315e3f2e3df168fba4a
이준석 <boris0730@gmail.com>
2021-03-03 19:13:07 +0900

    2981

--- c++/2981(검문)/2981.cpp
+++ c++/2981(검문)/2981.cpp
@@ -1,10 +1,59 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+int arr[101];
+int sol[500];
+
+int gcd(int a, int b)
+{
+    int tmp;
+    if( a > b)
+    {
+        while(b)
+        {
+            tmp = a % b;
+            a = b ;
+            b = tmp;
+        }
+        return a;
+    }
+    else
+    {
+        while(a)
+        {
+            tmp = b % a;
+            b = a;
+            a = tmp;
+        }
+        return b;
+    }
+}
+
 int main()
 {
-	int n;
+	int n,m,cnt=0;
 	cin >> n;
-
+	for(int i =0; i < n; i++)
+		cin >> arr[i];
+	sort(arr, arr + n);
+	m = arr[1]-arr[0];
+	for(int i =2 ; i < n; i++)
+	{
+		m = gcd(m,arr[i] - arr[i-1]);
+	}
+	for(int i = 1; i*i <= m;i++)
+	{
+		if( m % i == 0)
+		{
+			sol[cnt++] = i;
+			if ( i != m / i)
+				sol[cnt++] = m /i;
+		}
+	}
+	sort(sol,sol+cnt);
+	for(int i = 0; i< cnt; i++)
+		if(sol[i] != 1)
+			cout << sol[i] << " ";
 }

2229e6cfc76b7ffb5ede5ce8c238468a81d74dee
이준석 <boris0730@gmail.com>
2021-03-09 21:38:32 +0900

    3036

--- c++/3036(링)/3036.cpp
+++ c++/3036(링)/3036.cpp
@@ -1,1 +1,44 @@
 #include <iostream>
+
+using namespace std;
+
+int arr[100];
+
+int gcd(int a, int b)
+{
+    int tmp;
+    if( a > b)
+    {
+        while(b)
+        {
+            tmp = a % b;
+            a = b ;
+            b = tmp;
+        }
+        return a;
+    }
+    else
+    {
+        while(a)
+        {
+            tmp = b % a;
+            b = a;
+            a = tmp;
+        }
+        return b;
+    }
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	cin >> arr[0];
+	int tmp;
+	for (int i = 1; i< t; i++)
+	{
+		cin >> arr[i];
+		tmp = gcd(arr[0],arr[i]);
+		cout << arr[0] / tmp << "/" << arr[i] / tmp << '\n';
+	}
+}

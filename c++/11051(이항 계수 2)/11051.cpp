28a2d1f8869d31a81f164a7ae8a590f5e7a1e4a9
이준석 <boris0730@gmail.com>
2021-03-29 15:31:21 +0900

    11051

--- c++/11051(이항 계수 2)/11051.cpp
+++ c++/11051(이항 계수 2)/11051.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
+int dp[1001][1001];
+
 int main()
 {
-
+	int n , m;
+	cin >> n >> m;
+	for(int i = 1; i <=n; i++)
+	{
+		for(int j = 0; j <= m; j++)
+		{
+			if(i == j || j == 0)
+			{
+				dp[i][j] = 1;
+				continue;
+			}
+			dp[i][j] = (dp[i-1][j]+dp[i-1][j-1]) % 10007;
+		}
+	}
+	cout << dp[n][m] << '\n';
 }

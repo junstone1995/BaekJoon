792d4cfa0119fe2994afb992161ec743a95671d8
이준석 <boris0730@gmail.com>
2021-03-31 15:22:47 +0900

    18258

---
+++ c++/18258(큐 2)/18258.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    queue<int> q;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        string str;
+        cin >> str;
+        if (str == "push")
+        {
+            int num;
+            cin >> num;
+            q.push(num);
+        }
+        else if (str == "pop")
+        {
+            if (q.empty())
+                cout << "-1\n";
+            else
+            {
+                cout << q.front() <<'\n';
+                q.pop();
+            }
+        }
+        else if (str == "front")
+        {
+            if(q.empty())
+            {
+                cout << "-1\n";
+            }
+            else
+            {
+                cout << q.front() << "\n";
+            }
+        }
+        else if (str == "back")
+        {
+            if(q.empty())
+            {
+                cout << "-1\n";
+            }
+            else
+            {
+                cout << q.back() << "\n";
+            }
+        }
+        else if (str == "empty")
+        {
+            if(q.empty())
+                cout << "1\n";
+            else
+                cout << "0\n";
+        }
+        else
+            cout << q.size() << "\n";
+    }
+}

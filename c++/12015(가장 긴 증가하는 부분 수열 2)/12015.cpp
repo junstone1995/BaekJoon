2e8347886d481ca8c79e44cd59a96c292fcdd520
Junseok Lee <boris0730@gmail.com>
2021-04-09 12:27:54 +0900

    1300

---
+++ c++/12015(가장 긴 증가하는 부분 수열 2)/12015.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int
+int main()
+{
+	cin >> n;
+
+}
---
+++ c++/1300(K번째 수)/1300.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int k;
+int main()
+{
+	cin >> n >> k;
+	int left(1),right(k),ans;
+	while(left <= right)
+	{
+		long long cnt = 0;
+		int mid = (left + right) / 2;
+		for(int i = 1; i< n; i++)
+			cnt += min(mid / i ,n);
+		if(cnt < k)
+			left = mid + 1;
+		else
+		{
+			right = mid - 1;
+			ans = mid;
+		}
+	}
+	cout << ans << '\n';
+}

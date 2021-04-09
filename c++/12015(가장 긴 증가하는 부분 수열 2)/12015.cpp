569d7ab4fa13b04ef432401621c43b8e90895aa7
Junseok Lee <boris0730@gmail.com>
2021-04-09 13:18:54 +0900

    12015

--- c++/12015(가장 긴 증가하는 부분 수열 2)/12015.cpp
+++ c++/12015(가장 긴 증가하는 부분 수열 2)/12015.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int n;
-int
+vector<int> v;
 int main()
 {
 	cin >> n;
-
+	v.push_back(0);
+	for (int i= 0; i< n; i++)
+	{
+		int num;
+		cin >> num;
+		if(num > v.back())
+		{
+			v.push_back(num);
+		}
+		else
+		{
+			int = index = lower_bound(v.begin,v.end(),num) - v.begin();
+			v[index] = num;
+		}
+	}
+	cout << v.size() - 1 << '\n';
 }

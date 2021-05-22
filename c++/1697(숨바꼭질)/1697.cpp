30a51800d1ed6642f031a65be3150189f90516ac
Junseok Lee <boris0730@gmail.com>
2021-05-22 15:53:23 +0900

    1

---
+++ c++/1697(숨바꼭질)/1697.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+#define MAX 100001
+
+int n,k;
+bool visit[MAX];
+int cnt;
+
+int		BFS(int n, int k)
+{
+	queue<pair <int , int> > q;
+	q.push(make_pair(n,0));
+	visit[n] = true;
+
+	while(!q.empty())
+	{
+		int line = q.front().first;
+		int time = q.front().second;
+		q.pop();
+
+		if (line = k)
+			return time;
+		if (line + 1 < MAX && !visit[line+1])
+		{
+			q.push(make_pair(line+1, time +1));
+			visit[line + 1] = true;
+		}
+		if (line - 1 < MAX && !visit[line-1])
+		{
+			q.push(make_pair(line -1 ,time + 1));
+			visit[line - 1] = true;
+		}
+		if (line * 2 < MAX && !visit[line * 2])
+		{
+			q.push(make_pair(line * 2, time + 1));
+			visit[line * 2] = true;
+		}
+	}
+}
+
+int main()
+{
+	cin >> n >> k;
+
+	cout << BFS(n,k) << '\n';
+}

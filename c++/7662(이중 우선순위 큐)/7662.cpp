0f80f386634cff0b79ad695f94c574503eb4b087
Junseok Lee <boris0730@gmail.com>
2021-05-25 21:46:14 +0900

    7662

---
+++ c++/7662(이중 우선순위 큐)/7662.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+	int T;
+
+
+	cin >> T;
+	int k;
+	while(T--)
+	{
+		cin >> k;
+		char c;
+		int i;
+		multiset<int> mq;
+		while (k--)
+		{
+			cin >> c >> i;
+			if (c == 'I')
+				mq.insert(i);
+			else
+			{
+				if(!mq.empty() && i == -1)
+					mq.erase(mq.begin());
+				else if (!mq.empty() && i == 1)
+				{
+					auto iter = mq.end();
+					iter--;
+					mq.erase(iter);
+				}
+			}
+		}
+		if (mq.empty())
+			cout << "EMPTY\n";
+		else
+		{
+			auto end_iter = mq.end();
+			end_iter--;
+			cout << *end_iter << ' ' << *mq.begin() << '\n';
+		}
+	}
+}

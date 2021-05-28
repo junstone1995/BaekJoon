10d0753c5d6837cdef451179fba3123f8c770fa9
Junseok Lee <boris0730@gmail.com>
2021-05-28 17:11:37 +0900

    1260

--- c++/1260(DFS와 BFS)/1260.cpp
+++ c++/1260(DFS와 BFS)/1260.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cstring>
 
-#define MAX 10001
+#define MAX 1001
 using namespace std;
 
 int n,m,v;
-vector<int> ret[MAX];
+int ret[MAX][MAX];
 bool visit[MAX];
-
+queue<int> q;
 void	bfs(int idx)
 {
-
+	q.push(idx);
+	visit[idx] = true;
+	while(!q.empty())
+	{
+		idx = q.front();
+		cout << idx << ' ';
+		q.pop();
+		for(int i = 1; i <= n ; i++)
+		{
+			if(visit[i] == 1 || ret[idx][i] == 0)
+				continue;
+				q.push(i);
+				visit[i] = true;
+		}
+	}
 }
 
 void	dfs(int idx)
@@ -20,7 +35,7 @@ void	dfs(int idx)
 	visit[idx] = true;
 	for(int i = 1; i<= n; i++)
 	{
-		if(visit[i] == 1 || ret[i].front() == 0)
+		if(visit[i] == 1 || ret[idx][i] == 0)
 			continue;
 		dfs(i);
 	}
@@ -29,5 +44,15 @@ void	dfs(int idx)
 int main()
 {
 	cin >> n >> m >> v;
-
+	int x,y;
+	for (int i = 0; i< m; i++)
+	{
+		cin >> x >> y;
+		ret[x][y] = 1;
+		ret[y][x] = 1;
+	}
+	dfs(v);
+	cout << '\n';
+	memset(visit, false, MAX);
+	bfs(v);
 }

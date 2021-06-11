6f1c65e67d0d62e034d60cf9a3899fd1c27fff40
Junseok Lee <boris0730@gmail.com>
2021-06-11 23:50:29 +0900

    1043

---
+++ c++/1043(거짓말)/1043.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n,m,k;
+int parent[51];
+vector<int> know;
+vector<vector <int > > v(50);
+
+int Find(int x)
+{
+    if (parent[x] == x)
+        return x;
+    return x = Find(parent[x]);
+}
+
+void Union(int x,int y)
+{
+    x = Find(x);
+    y = Find(y);
+    parent[x] = y;
+}
+
+int main()
+{
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin >> n >> m >> k;
+    while (k--)
+    {
+        int t;
+        cin >> t;
+        know.push_back(t);
+    }
+    for(int i = 1; i<=n ; i++)
+        parent[i] = i;
+    for(int i= 0; i< m ; i++)
+    {
+        int p;
+        cin >> p;
+        int num;
+        int prev;
+        for (int j = 0; j < p ; j++)
+        {
+            if ( j >= 1)
+            {
+                prev = num;
+                cin >> num;
+                Union(prev, num);
+            }
+            else
+                cin >> num;
+            v[i].push_back(num);
+        }
+    }
+    for(auto& list:v)
+    {
+        bool flag = false;
+        for(auto& person : list)
+        {
+            if (flag)
+                break;
+            for (auto& t : know)
+            {
+                if(Find(person) == Find(t))
+                {
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        if (flag)
+            m--;
+    }
+    cout << m << '\n';
+}
